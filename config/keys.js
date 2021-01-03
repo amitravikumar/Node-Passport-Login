a3d5bd6d77c43aabec95f724d2b41a54648a1c15
Amit Kumar <besttechamit@gmail.com>
2021-01-03 15:39:56 +0530

    Project completed and all files updated

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mycluster.thdgt.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1ulkh.mongodb.net/${process.env.DB_COLLECTION}?retryWrites=true&w=majority'
 }
